5a06ca7409fb3ae817278755aa1cda53d5e99b3e
JhoneApMoraes <jhoneapmoraes@gmail.com>
2020-12-14 22:03:21 -0300

    first commit

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+private Transform BackPoint;
+    void Start()
+    {
+       BackPoint = GameObject.Find("BackPoint").GetComponent<Transform>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < BackPoint.position.x)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformGeneration.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGeneration : MonoBehaviour
+{
+    public GameObject Platform; 
+    public Transform point;
+    public float Distance;
+
+    private float plataforWidth;
+
+    void Start()
+    {
+        if (Platform.GetComponent<BoxCollider2D>())
+        {
+            plataforWidth = Platform.GetComponent<BoxCollider2D>().size.x;
+        }
+        else
+        {
+            plataforWidth = Platform.GetComponent<PolygonCollider2D>().bounds.size.x;
+
+        }
+    }
+
+    void Update()
+    {
+        //logica para gerar plataformas 
+        if (transform.position.x < point.position.x)
+        {
+
+            Distance = Random.Range(3f, 8f);
+
+            transform.position = new Vector3(transform.position.x + plataforWidth + Distance, transform.position.y, transform.position.z);
+            Instantiate(Platform, transform.position, transform.rotation);
+        }
+    }
+}
